a2b41e136fa6750964106759b3479e4c51f4f5b5
ALwtg <3063869928@qq.com>
2026-01-14 13:29:37 +0800

    Update

--- config.js
+++ config.js
@@ -1,20 +1,25 @@
 const CONFIGS = {
-    'gemini-3-flash': {
-        url: 'https://api.chatanywhere.tech/v1/chat/completions',
+    'gemini_3_flash': {
+        url: 'https://api.chatanywhere.tech/v1/chat/completions  ',
         key: 'sk-',
         model: 'gemini-3-flash-preview'
     },
-    'gemini-2.5-pro': {
-        url: 'https://api.chatanywhere.tech/v1/chat/completions',
+    'gemini_3_flash_X666_': {
+        url: 'https://x666.me/v1/chat/completions',
         key: 'sk-',
-        model: 'gemini-2.5-pro'
+        model: 'gemini-3-flash-preview'
     },
-    'glm-4.6v': {
+    'gemini_3_flash_BLT': {
+        url: 'https://api.bltcy.ai/v1/chat/completions',
+        key: 'sk-',
+        model: 'gemini-3-flash-preview'
+    },
+    'glm_4.6v': {
         url: 'https://api.siliconflow.cn/v1/chat/completions',
         key: 'sk-',
         model: 'zai-org/GLM-4.6V'
     },
-    'qwen-32b': {
+    'qwen_32b': {
         url: 'https://api.siliconflow.cn/v1/chat/completions',
         key: 'sk-',
         model: 'Qwen/Qwen3-VL-32B-Thinking'
