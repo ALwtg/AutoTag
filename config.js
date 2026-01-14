614c187a586dfd8318360bdf9de5dd0c4a8eaf5e
ALwtg <3063869928@qq.com>
2026-01-14 13:44:55 +0800

    Update

--- config.js
+++ config.js
@@ -1,27 +1,27 @@
 const CONFIGS = {
     'gemini_3_flash': {
-        url: 'https://x666.me/v1/chat/completions',
-        key: 'sk-JSXcgsMAzBaOny7f3mlqwoogqDj53gSsdjhDjvO3BeDzf9TE',
+        url: 'https://api.chatanywhere.tech/v1/chat/completions',
+        key: 'sk-',
         model: 'gemini-3-flash-preview'
     },
     'gemini_3_flash_X666_': {
         url: 'https://x666.me/v1/chat/completions',
-        key: 'sk-JSXcgsMAzBaOny7f3mlqwoogqDj53gSsdjhDjvO3BeDzf9TE',
+        key: 'sk-',
         model: 'gemini-3-flash-preview'
     },
     'gemini_3_flash_BLT': {
         url: 'https://api.bltcy.ai/v1/chat/completions',
-        key: 'sk-3tiphROte97DQz4tx21HGUv4NEiSL34ziWDIgZVdsr93F3ao',
+        key: 'sk-',
         model: 'gemini-3-flash-preview'
     },
     'glm_4.6v': {
         url: 'https://api.siliconflow.cn/v1/chat/completions',
-        key: 'sk-ydevmqaikiyikpxtpifoavgvrqcemuusizzzqzoryoooismv',
+        key: 'sk-',
         model: 'zai-org/GLM-4.6V'
     },
     'qwen_32b': {
         url: 'https://api.siliconflow.cn/v1/chat/completions',
-        key: 'sk-ydevmqaikiyikpxtpifoavgvrqcemuusizzzqzoryoooismv',
+        key: 'sk-',
         model: 'Qwen/Qwen3-VL-32B-Thinking'
     }
 };
